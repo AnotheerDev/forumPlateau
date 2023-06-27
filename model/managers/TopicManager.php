81656e584ae2a5d5a387ba09d7e4747f7ad8b0db
AnotheerDev <leger.julien94@gmail.com>
2023-06-27 16:42:51 +0200

    add function fetchTopicsByCat

--- model/managers/TopicManager.php
+++ model/managers/TopicManager.php
@@ -16,4 +16,21 @@ class TopicManager extends Manager
     {
         parent::connect();
     }
+
+    public function fetchTopicsByCat($id)
+    {
+
+        $sql = "SELECT id_topic, title, dateCreation, locked, member_id, category_id
+                    FROM " . $this->tableName . " t
+                    LEFT JOIN post p ON p.topic_id = t.id_" . $this->tableName . "
+                    WHERE t.category_id = :id
+                    GROUP BY t.id_" . $this->tableName . "
+                    ORDER BY t.publishDate DESC";
+
+
+        return $this->getMultipleResults(
+            DAO::select($sql, ['id' => $id]),
+            $this->className
+        );
+    }
 }
