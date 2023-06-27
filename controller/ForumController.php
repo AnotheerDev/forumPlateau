d672972f9202ccab9203d9499186882b4c5818b0
AnotheerDev <leger.julien94@gmail.com>
2023-06-27 11:40:51 +0200

    Add function ListCategories

--- controller/ForumController.php
+++ controller/ForumController.php
@@ -7,6 +7,8 @@ use App\AbstractController;
 use App\ControllerInterface;
 use Model\Managers\TopicManager;
 use Model\Managers\PostManager;
+use Model\Managers\MemberManager;
+use Model\Managers\CategoryManager;
 
 class ForumController extends AbstractController implements ControllerInterface
 {
@@ -21,6 +23,19 @@ class ForumController extends AbstractController implements ControllerInterface
             "view" => VIEW_DIR . "forum/listTopics.php",
             "data" => [
                 "topics" => $topicManager->findAll(["dateCreation", "DESC"])
+            ]
+        ];
+    }
+
+
+    public function listCategories()
+    {
+        $categoryManager = new CategoryManager();
+
+        return [
+            "view" => VIEW_DIR . "forum/listCategories.php",
+            "data" => [
+                "categories" => $categoryManager->findAll(["Name", "ASC"])
             ]
         ];
     }
