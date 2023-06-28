f4153295c2b1182ffd101e4be554fc0a800bb15b
AnotheerDev <leger.julien94@gmail.com>
2023-06-28 11:39:47 +0200

    rectification a href

--- view/forum/listCategories.php
+++ view/forum/listCategories.php
@@ -9,7 +9,7 @@ $categories = $result["data"]['categories'];
 <?php
 foreach ($categories as $category) {
 ?>
-    <a href="index.php?ctrl=forum&action=listTopicsByCat&id<?= $category->getId() ?>">
+    <a href="index.php?ctrl=forum&action=listTopicsByCat&id=<?= $category->getId() ?>">
         <p><?= $category->getName() ?></p>
     </a>
 
