407e211e35dcbea2717e6de73ef879719e099706
AnotheerDev <leger.julien94@gmail.com>
2023-06-28 11:39:03 +0200

    Add liste topics par cat

--- view/forum/listTopicsByCat.php
+++ view/forum/listTopicsByCat.php
@@ -1,1 +1,23 @@
 <?php
+$category = $result["data"]['category'];
+$topics = $result["data"]['topics'];
+
+?>
+
+<h1>liste topics de la catégorie <?= $category->getName() ?></h1>
+
+<?php
+if ($topics) {
+    foreach ($topics as $topic) {
+?>
+
+        <p><?= $topic->getTitle() ?></p>
+        <p>Créé le : <?= $topic->getDateCreation() ?></p>
+        <p>Créé par : <?= $topic->getMember()->getNickname() ?></p>
+
+<?php
+    }
+} else {
+    echo "<h1>Pas de topic dans la catégorie</h1>";
+}
+?>
