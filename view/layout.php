c10d4267e18e6d65bd8812e114e13bf84388116b
AnotheerDev <leger.julien94@gmail.com>
2023-06-29 15:02:48 +0200

    redirection accueil sur Home

--- view/layout.php
+++ view/layout.php
@@ -34,7 +34,7 @@
                             <li class="nav-item dropdown">
                                 <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Home</a>
                                 <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="/">Accueil</a>
+                                    <a class="dropdown-item" href="index.php?ctrl=home&action=home">Accueil</a>
                                     <?php
                                     if (App\Session::isAdmin()) {
                                     ?>
