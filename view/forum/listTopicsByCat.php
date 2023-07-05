36a9f762415bf0b22ce4bdb93ad4be729823a57e
AnotheerDev <leger.julien94@gmail.com>
2023-07-05 16:44:42 +0200

    add Session::getUser && btn for lock/unlock

--- view/forum/listTopicsByCat.php
+++ view/forum/listTopicsByCat.php
@@ -19,6 +19,13 @@ $topics = $result["data"]['topics'];
                         <div class="topic-box-info">
                             <p>Créé le : <?= $topic->getDateCreation() ?></p>
                             <p>Créé par : <?= $topic->getUser()->getNickname() ?></p>
+                            <?php if ((App\Session::getUser() && App\Session::getUser() == $topic->getUser() || App\Session::isAdmin())) : ?>
+                                <?php if ($topic && $topic->getLocked()) : ?>
+                                    <a href="index.php?ctrl=forum&action=changeStatusTopic&id=<?= $topic->getId() ?>" class="update-button" onclick="return confirm('Êtes-vous sûr de vouloir déverrouiller ce topic ?')">Unlock</a>
+                                <?php else : ?>
+                                    <a href="index.php?ctrl=forum&action=changeStatusTopic&id=<?= $topic->getId() ?>" class="delete-button" onclick="return confirm('Êtes-vous sûr de vouloir verrouiller ce topic ?')">Lock</a>
+                                <?php endif; ?>
+                            <?php endif; ?>
                         </div>
                     </div>
                 </a>
@@ -31,22 +38,22 @@ $topics = $result["data"]['topics'];
     </div>
 
     <?php if (App\Session::getUser() && App\Session::getUser()->getId()) : ?>
-    <div>
-        <div class="new-topic-form">
-            <h2>Ajouter un nouveau topic</h2>
+        <div>
+            <div class="new-topic-form">
+                <h2>Ajouter un nouveau topic</h2>
 
-            <form action="index.php?ctrl=forum&action=addTopic&id=<?= $category->getId() ?>" method="POST">
-                <label for="title">Titre du topic :</label>
-                <input type="text" name="title" id="title" placeholder="Entrez le titre du topic" required>
+                <form action="index.php?ctrl=forum&action=addTopic&id=<?= $category->getId() ?>" method="POST">
+                    <label for="title">Titre du topic :</label>
+                    <input type="text" name="title" id="title" placeholder="Entrez le titre du topic" required>
 
-                <label for="post">Message :</label>
-                <textarea name="post" id="post" rows="4" placeholder="Entrez votre message" required></textarea>
+                    <label for="post">Message :</label>
+                    <textarea name="post" id="post" rows="4" placeholder="Entrez votre message" required></textarea>
 
-                <input type="submit" name="submit" value="Ajouter">
-            </form>
+                    <input type="submit" name="submit" value="Ajouter">
+                </form>
+            </div>
         </div>
-    </div>
-<?php endif; ?>
+    <?php endif; ?>
 
 
 </div>
